7124096d20025185ce0085f24a5541cbcfa43512
Jason Long <jlong@github.com>
2014-01-25 15:11:17 -0500

    sine waves working

--- geopatterns.rb
+++ geopatterns.rb
@@ -11,7 +11,7 @@ class SVG
     @width = width
   end
 
-  def set_height
+  def set_height(height)
     @height = height
   end
 
@@ -35,7 +35,7 @@ class SVG
     str = ""
     args.each {|key, value|
       if value.is_a?(Hash)
-        str << %Q{class="}
+        str << %Q{#{key}="}
         value.each {|key, value|
           str << %Q{#{key}:#{value};}
         }
@@ -61,20 +61,45 @@ class GeoPattern
   end
 
   def geoSineWaves
-    fill = "#dddddd"
-    opacity = 0.02
-    wave_width = "30px"
+    period     = map(@hash[1, 1].to_i(16), 0, 15, 100, 400).floor
+    amplitude  = map(@hash[2, 1].to_i(16), 0, 15, 30, 100).floor
+    wave_width = map(@hash[3, 1].to_i(16), 0, 15, 3, 30).floor
 
-    @svg.path("M0 44 C 42 0, 78 0, 120 44 S 198 88, 240 44 S 318 0, 360, 44",
-              {
-                "fill" => "none",
-                "stroke" => fill,
-                # transform: "t-"+period/4+","+(waveWidth*i-amplitude*1.5),
-                "style" => {
-                  "opacity" => opacity,
-                  "stroke-width" => wave_width
-                }
-              })
+    @svg.set_width(period)
+    @svg.set_height(wave_width * 36)
+
+    for i in 0..35
+      val      = @hash[i, 1].to_i(16)
+      fill     = (val % 2 == 0) ? "#ddd" : "#222";
+      opacity  = map(val, 0, 15, 0.02, 0.15);
+      x_offset = period / 4 * 0.7;
+
+      str = "M0 "+amplitude.to_s+
+            " C "+x_offset.to_s+" 0, "+(period/2 - x_offset).to_s+" 0, "+(period/2).to_s+" "+amplitude.to_s+
+            " S "+(period-x_offset).to_s+" "+(amplitude*2).to_s+", "+period.to_s+" "+amplitude.to_s+
+            " S "+(period*1.5-x_offset).to_s+" 0, "+(period*1.5).to_s+", "+amplitude.to_s;
+
+      @svg.path(str, {
+                  "fill" => "none",
+                  "stroke" => fill,
+                  transform: "translate(-#{period/4}, #{wave_width*i-amplitude*1.5})",
+                  "style" => {
+                    "opacity" => opacity,
+                    "stroke-width" => "#{wave_width}px"
+                  }
+                })
+
+      @svg.path(str, {
+                  "fill" => "none",
+                  "stroke" => fill,
+                  transform: "translate(-#{period/4}, #{wave_width*i-amplitude*1.5 + wave_width*36})",
+                  "style" => {
+                    "opacity" => opacity,
+                    "stroke-width" => "#{wave_width}px"
+                  }
+                })
+    end
+
     # @svg << "<path d='M0 44 C 42 0, 78 0, 120 44 S 198 88, 240 44 S 318 0, 360, 44' fill='none' stroke='#dddddd' transform='matrix(1,0,0,1,-60,1014)' style='opacity: 0.02; stroke-width: 30px;'/>"
   end
 
