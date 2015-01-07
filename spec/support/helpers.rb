051608bbcbc1b4c2cf5f32a9a64be55080395953
Max Meyer <dev@fedux.org>
2015-01-07 23:03:49 +0100

    Added silence helper from active support to reduce noise from tests

--- spec/support/helpers.rb
+++ spec/support/helpers.rb
@@ -1,4 +1,36 @@
 module SpecHelpers
+  # Taken from active support
+  #
+  # Captures the given stream and returns it:
+  #
+  # stream = capture(:stdout) { puts 'notice' }
+  # stream # => "notice\n"
+  #
+  # stream = capture(:stderr) { warn 'error' }
+  # stream # => "error\n"
+  #
+  # even for subprocesses:
+  #
+  # stream = capture(:stdout) { system('echo notice') }
+  # stream # => "notice\n"
+  #
+  # stream = capture(:stderr) { system('echo error 1>&2') }
+  # stream # => "error\n"
+  def capture(stream)
+    stream = stream.to_s
+    captured_stream = Tempfile.new(stream)
+    stream_io = eval("$#{stream}")
+    origin_stream = stream_io.dup
+    stream_io.reopen(captured_stream)
+    yield
+    stream_io.rewind
+    return captured_stream.read
+  ensure
+    captured_stream.close
+    captured_stream.unlink
+    stream_io.reopen(origin_stream)
+  end
+  alias :silence :capture
 end
 
 RSpec.configure do |config|
