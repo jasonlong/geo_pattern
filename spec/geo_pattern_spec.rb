dd230afa73678b110ab5ea694456c3efa0158e12
Max Meyer <dev@fedux.org>
2015-01-08 18:55:16 +0100

    Use let for string

--- spec/geo_pattern_spec.rb
+++ spec/geo_pattern_spec.rb
@@ -2,21 +2,22 @@
 require 'spec_helper'
 
 RSpec.describe GeoPattern do
+  let(:string) { 'Mastering Markdown' }
+
   context '.generate' do
     it 'generates a pattern for a string' do
-      pattern = GeoPattern.generate('Mastering Markdown')
+      pattern = GeoPattern.generate(string)
       expect(pattern).not_to be_nil
     end
 
     it 'is always the same' do
-      pattern1 = GeoPattern.generate('Mastering Markdown')
-      pattern2 = GeoPattern.generate('Mastering Markdown')
+      pattern1 = GeoPattern.generate(string)
+      pattern2 = GeoPattern.generate(string)
 
       expect(pattern1.svg_string).to eq pattern2.svg_string
     end
 
     it 'sets background color with adjusting hue and saturation based on string' do
-      string = 'Mastering Markdown'
       html_base_color = '#fc0'
       rgb_base_color  = html_to_rgb_for_string(string, html_base_color)
       pattern         = GeoPattern.generate(string, base_color: html_base_color)
@@ -25,7 +26,6 @@ RSpec.describe GeoPattern do
     end
 
     it 'sets background color' do
-      string = 'Mastering Markdown'
       html_base_color = '#fc0'
       rgb_base_color  = html_to_rgb(html_base_color)
       pattern         = GeoPattern.generate(string, color: html_base_color)
@@ -34,7 +34,6 @@ RSpec.describe GeoPattern do
     end
 
     it 'uses the specified generator' do
-      string   = 'Mastering Markdown'
 
       pattern1 = GeoPattern.generate(string, generator: GeoPattern::SineWavePattern)
       pattern2  = GeoPattern.generate(string)
