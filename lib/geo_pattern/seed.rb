00d156f4fb01c0a7e9856719a8f34c591f672c0c
Max Meyer <dev@fedux.org>
2015-01-26 21:54:35 +0100

    Remove not needed method + tests from seed

--- lib/geo_pattern/seed.rb
+++ lib/geo_pattern/seed.rb
@@ -10,10 +10,6 @@ module GeoPattern
       @seed = Digest::SHA1.hexdigest string.to_s
     end
 
-    def to_s
-      seed
-    end
-
     def [](*args)
       seed[*args]
     end
--- spec/seed_spec.rb
+++ spec/seed_spec.rb
@@ -6,17 +6,6 @@ RSpec.describe Seed do
 
   it { expect(seed).not_to be_nil }
 
-  describe '#to_s' do
-    context 'when input is a string' do
-      it { expect(seed.to_s).to be_kind_of String }
-    end
-
-    context 'when input is a integer' do
-      let(:input) { 1 }
-      it { expect(seed.to_s).to be_kind_of String }
-    end
-  end
-
   describe '#[]' do
     context 'when use an integer' do
       it { expect(seed[1]).to be_kind_of String }
