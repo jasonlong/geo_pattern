431700bef2e2fe55c2c43ace1626dde717969733
Max Meyer <dev@fedux.org>
2015-02-01 22:36:56 +0100

    Add test to use fixtures for testing

--- spec/support/shared_examples/pattern.rb
+++ spec/support/shared_examples/pattern.rb
@@ -1,5 +1,11 @@
 shared_examples 'a chosen pattern' do |name|
   subject(:pattern) { GeoPattern.generate(input, patterns: name) }
+  let(:file_name) { "#{name}.svg" }
+
+  before :each do
+    write_file file_name, pattern.to_s
+  end
 
   it { expect(pattern.structure).to be_name name }
+  it { check_binary_file_content(file_name, fixtures_path("generated_patterns/#{name}.svg")) }
 end
