b79aaa0c62eaeba487e78204da0039da0cf46e78
Jason Long <jlong@github.com>
2020-07-24 07:10:17 -0400

    Use aruba/rspec

--- spec/support/aruba.rb
+++ spec/support/aruba.rb
@@ -1,25 +1,3 @@
 # frozen_string_literal: true
 
-require 'aruba/api'
-require 'aruba/reporting'
-
-# Spec Helpers
-module SpecHelper
-  # Helpers for aruba
-  module Aruba
-    include ::Aruba::Api
-
-    def dirs
-      @dirs ||= %w[tmp rspec]
-    end
-  end
-end
-
-RSpec.configure do |c|
-  c.include SpecHelper::Aruba
-
-  c.before :each do
-    setup_aruba
-    restore_env
-  end
-end
+require 'aruba/rspec'
